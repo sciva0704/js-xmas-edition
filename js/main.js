b057ff42ec16fe2dcfa390be1b83b02f2d7cd666
sciva0704 <santiagocivale@gmail.com>
2024-12-27 13:00:49 -0300

    Funcion de errores cuando se envia formulario
    Toda la validacion para que cuando se envia algo incorrecto funcione

--- js/main.js
+++ js/main.js
@@ -1,16 +1,12 @@
-const $form = document.querySelector('#carta-a-santa');
-
-const nombre = $form.nombre.value;
-const ciudad = $form.ciudad.value;
-const comportamiento = $form.comportamiento.value;
-const descripcionRegalo = $form['descripcion-regalo'].value;
-
 function validarNombre(nombre){
     if (nombre.length === 0) {
         return 'Este campo debe tener al menos 1 caracter';
     }
-    if (nombre.length > 50) {
+    if (nombre.length >= 50) {
         return 'Este campo debe tener menos de 50 caracteres';
+    }
+    if (!/^[a-z]+$/i.test(nombre)) {
+        return 'El campo solo acepta Letras';
     }
     return '';
 }
@@ -26,10 +22,65 @@ function validarCiudad(ciudad) {
 function validarDescripcionRegalo(descripcionRegalo) {
     if (descripcionRegalo.length === 0) {
         return 'Este campo debe tener al menos 1 caracter';
-    } else if (descripcionRegalo > 50) {
+    } 
+    if (descripcionRegalo.length >= 50) {
         return 'Este campo debe tener menos de 50 caracteres';
-    }  else {
+    }
+    if (!/^[A-z]+$/i.test(descripcionRegalo)) {
+        return 'El campo descripción regalo solo acepta números y letras';
+    } else {
         return '';
     }
 }
 
+function validarForm(e){
+    e.preventDefault();
+    const $form = document.querySelector('#carta-a-santa');
+    const nombre = $form.nombre.value;
+    const ciudad = $form.ciudad.value;
+    const descripcionRegalo = $form['descripcion-regalo'].value;
+
+    const errorNombre = validarNombre(nombre);
+    const errorCiudad = validarCiudad(ciudad);
+    const errorDescripcionRegalo = validarDescripcionRegalo(descripcionRegalo);
+
+    const errores = {
+        nombre: errorNombre,
+        ciudad: errorCiudad,
+        'descripcion-regalo': errorDescripcionRegalo,
+    };
+
+
+    const esExito = manejarErrores(errores) === 0;
+
+    if (esExito) {
+        document.querySelector('#exito').className = "";
+    }
+}
+
+function manejarErrores(errores){
+    const keys = Object.keys(errores);
+    const $errores = document.querySelector('#errores');
+    let cantidadErrores = 0;
+
+    keys.forEach(function(key){
+        const error = errores[key];
+        
+        if (error) {
+            cantidadErrores++;
+            $form[key].className = "error";
+
+            const $error = document.createElement('li');
+            $error.innerText = error;
+            $errores.appendChild($error);
+        } else {
+            $form[key].className = "";
+        }
+
+        
+    })
+    return cantidadErrores;
+}
+
+const $form = document.querySelector('#carta-a-santa');
+$form.addEventListener('submit', validarForm);
--- js/pruebas.js
+++ js/pruebas.js
@@ -10,6 +10,16 @@ function probarValidarNombre() {
       'Este campo debe tener menos de 50 caracteres',
       'Validar nombre no validó que el nombre sea menor a 50 caracteres',
   );
+
+  console.assert(
+    validarNombre('123123') === 'El campo solo acepta Letras',
+        'ValidarNombre no valido que el campo solo acepte letras',
+  );
+
+  console.assert(
+    validarNombre('Santiago') === '',
+    'validarNombre fallo con un nombre valido',
+  );
 }
 
 function probarValidarCiudad() {
@@ -30,6 +40,16 @@ function probarValidarDescripcionRegalo() {
             '1111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111111') === 
             'Este campo debe tener menos de 50 caracteres',
             'validarDescripcionRegalo no validó que descripción regalo sea menor a 100 caracteres',
+    );
+
+    console.assert(
+        validarDescripcionRegalo('regalo') === '',
+        'validarDescripcionRegalo fallo con un texto válido'
+    )
+
+    console.assert(
+        validarDescripcionRegalo(',,,,,,,') === 'El campo descripción regalo solo acepta números y letras',
+        'la función validarDescripcionRegalo no validó que que solo se acepten números y letras',
     );
 }
 
